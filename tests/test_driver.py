0477b88b771ce5b04296bfdc7826149256202dc1
Alex Ruddick <alex@ruddick.tech>
2025-12-18 23:27:00 -0600

    Restore test since we don't use 3.9 any more

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -24,7 +24,6 @@ def test_driver_cli(capsys, unit):
     assert ("mass_flow" in captured.out)
 
 
-@pytest.mark.skip # broken on 3.9
 @pytest.mark.parametrize('cls', [FlowController])  # Fixme: fix FlowMeter
 async def test_is_connected(cls):
     """Confirm that connection status works."""
